a26550e09f1d997aceaa2098cff865a199babfdb
difsch2077 <127914304+difsch2077@users.noreply.github.com>
2025-05-17 22:33:52 +0800

    c

--- src/renderer/lib/stores.ts
+++ src/renderer/lib/stores.ts
@@ -2,11 +2,12 @@ import { writable } from 'svelte/store'
 import { getDefaultValues } from '../../common/stores'
 import type { StoreName, StoreSchemas, StoreValue } from '../../common/stores'
 
-
-// 创建强类型 Store 代理
 function createStore(name: StoreName): {
   subscribe: (run: (value: StoreValue) => void) => () => void
-  set: <K extends keyof StoreSchemas[StoreName]>(key: K, value: StoreSchemas[StoreName][K]) => Promise<void>
+  set: <K extends keyof StoreSchemas[StoreName]>(
+    key: K,
+    value: StoreSchemas[StoreName][K]
+  ) => Promise<void>
 } {
   const { subscribe, set } = writable<StoreSchemas[typeof name]>(getDefaultValues(name))
 
@@ -22,5 +23,5 @@ function createStore(name: StoreName): {
 }
 
 // 导出具体 Store
-export const configStore = createStore('config') 
+export const configStore = createStore('config')
 export const userStore = createStore('user')
