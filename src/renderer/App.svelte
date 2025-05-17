792b8b61e98970c0684fe980549cb6612caa0e10
difsch2077 <127914304+difsch2077@users.noreply.github.com>
2025-05-17 23:36:20 +0800

    从

--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -3,7 +3,10 @@
 
   // 直接使用 $ 语法自动订阅
   function toggleTheme(): void {
-    configStore.set('theme', $configStore.theme === 'light' ? 'dark' : 'light')
+    configStore.update((config) => ({
+      ...config,
+      theme: config.theme === 'light' ? 'dark' : 'light'
+    }))
   }
 </script>
 
--- src/renderer/lib/stores.ts
+++ src/renderer/lib/stores.ts
@@ -1,8 +1,8 @@
 import { Writable, writable } from 'svelte/store'
 import { getDefaultValues } from '../../common/stores'
-import type { StoreName, StoreSchemas, StoreValue } from '../../common/stores'
+import type { StoreName, StoreSchemas } from '../../common/stores'
 
-function createStore(name: StoreName): Writable<StoreSchemas[name]> {
+function createStore(name: StoreName): Writable<StoreSchemas[typeof name]> {
   const { subscribe, set } = writable<StoreSchemas[typeof name]>(getDefaultValues(name))
 
   // 初始化加载
@@ -10,15 +10,15 @@ function createStore(name: StoreName): Writable<StoreSchemas[name]> {
 
   return {
     subscribe,
-    set:( value: StoreSchemas[StoreName]) => {
+    set: (value: StoreSchemas[StoreName]) => {
       return window.electronStores.set(name, value)
     },
     update: (partial: Partial<StoreSchemas[typeof name]>) => {
       return window.electronStores.update(name, partial)
     }
-  }
+  } as Writable<StoreSchemas[typeof name]>
 }
 
 // 导出具体 Store
-export const configStore = createStore('config')
-export const userStore = createStore('user')
+export const configStore = createStore('config') as Writable<{ theme: 'light' | 'dark'; fontSize: number }>
+export const userStore = createStore('user') as Writable<{ id: string; name: string }>
